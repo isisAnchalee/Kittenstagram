2c9b27df799851425e6d53a8e2d41d8ce29949f3
Isis Anchalee <isiswenger@gmail.com>
2014-11-29 23:08:28 -0800

    adding edit profile features

--- app/assets/javascripts/views/edit_profile/edit_profile.js
+++ app/assets/javascripts/views/edit_profile/edit_profile.js
@@ -1,8 +1,12 @@
 Kittenstagram.Views.EditProfile = Backbone.CompositeView.extend({
   template: JST['users/edit_profile'],
-  
+
   initialize: function(){
     this.listenTo(this.model, 'sync', this.render);
+  },
+
+  events:{
+    "keyup .desc-input": "charsLeft"
   },
 
   render: function(){
@@ -12,6 +16,14 @@ Kittenstagram.Views.EditProfile = Backbone.CompositeView.extend({
 
     this.$el.html(renderedContent);
     return this;
+  },
+
+  charsLeft: function(event){
+    event.preventDefault();
+    var max_chars = 100,
+    chars_used = $('.desc-input').val().length,
+    chars_left = max_chars - chars_used;
+        $('#chars').text('You have ' + chars_left + ' characters remaining!');
   }
 
 });
