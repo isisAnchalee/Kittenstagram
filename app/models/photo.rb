f65fc5e96bf800948b3317762ac89b619d8300eb
Isis Anchalee <isiswenger@gmail.com>
2014-12-01 16:08:31 -0800

    Sorting order desc

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,12 +1,10 @@
 class Photo < ActiveRecord::Base
 	validates :user_id, :filepicker_url, presence:true
-	default_scope { order('created_at') } 
-	
+	 default_scope { order('created_at DESC') }
+
 	belongs_to :user
 	has_many :comments
 	has_many :likes
 	has_many :likers, through: :likes, source: :user
 end
 
-
-#default scope passing block-- block has ActiveRecord assiations order id descending
