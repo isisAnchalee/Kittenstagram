419957f823f68bd2637f199ed6db0b18d888100d
Isis Anchalee <isiswenger@gmail.com>
2014-12-01 20:12:03 -0800

    Styling edit photo page

--- app/assets/javascripts/views/users/gallery_image_show.js
+++ app/assets/javascripts/views/users/gallery_image_show.js
@@ -1,14 +1,12 @@
 Kittenstagram.Views.GalleryImageView = Backbone.View.extend({
   template: JST['users/gallery_image_view'],
-
   className: 'col-xs-3',
 
   render: function(){
-  	var likes = this.model.likes();
   	var renderedContent = this.template({
-  		photo: this.model,
-  		likes: likes
+  		photo: this.model
   	});
+
   	this.$el.html(renderedContent);
   	return this;
   }
